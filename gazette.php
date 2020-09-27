ce9dab72b1cd6eef20a9d714fb54ddc43d77ee24
Vincent Lammens <vincent@vincentlammens.be>
2020-09-27 18:52:10 +0200

    Update gazette.php
    added ban feature

--- gazette.php
+++ gazette.php
@@ -1,12 +1,12 @@
 <?php
-
 require_once('/path/to/mailparser.php');
-
 // Options begin
-
-$listaddress = "listname@example.com"; // will be from for frowarded msgs
+$listaddress = "list@example.be"; // will be from for frowarded msgs
 $subscribersfile = "/path/to/subscribers.txt";
-
+$listadmin = "admin@example.be"; // recieves notificications of subs
+$allowsubscribe = true;
+$bannedfile = "/path/to/banned.txt";
+$infofile = "/path/to/info.txt";
 // options end =================================================================
 
 // get data from STDIN
@@ -25,10 +25,19 @@ $from_address = substr(explode("<", $from)[1], 0, -1);
 // get the message body
 $body = $emailParser->getPlainBody();
 
+// check if the from address is in the banned file
+$banfile = file($bannedfile, FILE_IGNORE_NEW_LINES);
+if(array_search($from_address, $banfile)){
+  mail($listadmin,"Info for ".$listaddress,$from_address." tried to send, but is banned on list ".$listaddress.".","From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
+  die(); // the user is banned
+}
+
 // check if the subject line starts with subscribe if so, do the subscribe 
 // action (add the email to the subscriber file)
 if (substr(strtolower($subject), 0, 9) == "subscribe"){
-
+  if (!$allowsubscribe){
+    die(); // if subscribing is disabled, stop here
+  }
   $subscribers = array();
   $readfile = file($subscribersfile, FILE_IGNORE_NEW_LINES);
   foreach ($readfile as $subaddress)  {
@@ -39,7 +48,9 @@ if (substr(strtolower($subject), 0, 9) == "subscribe"){
   $addfile = fopen($subscribersfile, "a") or die("Unable to open file!");
   fwrite($addfile, $from_address."\n");
   fclose($addfile);
-  mail($from_address,"Re: ".$subject,"You are now subscribed to ".$listaddress.".","From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
+  $infocontent = file_get_contents($infofile);
+  mail($from_address,"Re: ".$subject,"You are now subscribed to ".$listaddress.".\n\n".$infocontent,"From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
+  mail($listadmin,"New subscriber on ".$listaddress,$from_address." is now subscribed to the list ".$listaddress.".","From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
   die(); // don't do anything else
 }
 
@@ -56,7 +67,6 @@ if ($issub == false){
   die(); // stop here, as the sender is not a subscriber
 }
 
-
 // check if the subject line starts with unsubscribe, if so, do the unsubscribe
 // action (remove the email from the subscriber file)
 if (substr(strtolower($subject), 0, 11) == "unsubscribe"){
@@ -64,13 +74,9 @@ if (substr(strtolower($subject), 0, 11) == "unsubscribe"){
   // that again here
   $newfile = fopen($subscribersfile, "w") or die("Unable to open file!");
   foreach ($readfile as $subaddress)  {
-    
     if ($subaddress != $from_address){
-      
       fwrite($newfile, $subaddress."\n");
-      
     }
-    
   }
   fclose($newfile);
   mail($from_address,"Re:". $subject,"You are now unsubscribed from ".$listaddress.".","From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
@@ -79,11 +85,9 @@ if (substr(strtolower($subject), 0, 11) == "unsubscribe"){
 
 // if the subject is nothing special, treat is as a mail that should be
 // forwarded to the subscribers. 
-
 foreach ($readfile as $subaddress)  {
   mail($subaddress,$subject,"Sender: ".$from."\n\n".$body,"From: ".$listaddress."\r\nX-Mailer: Gazette/0.1\r\nPrecedence: list\r\nList-Subscribe: <mailto:".$listaddress."?subject=subscribe>\r\nList-Unsubscribe: <mailto:".$listaddress."?subject=unsubscribe>");
 }
-
 // exit
 exit;
 ?>
